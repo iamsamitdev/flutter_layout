b45bf9472c81e5873e34c9db25fbafa44712c001
Samit Koyom <samitkoyom@gmail.com>
2020-12-15 14:37:24 +0700

    update menu

--- lib/main.dart
+++ lib/main.dart
@@ -5,14 +5,11 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.teal
-      ),
+      theme: ThemeData(primarySwatch: Colors.teal),
       home: HomePage(),
     );
   }
@@ -30,26 +27,85 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        leading: Icon(Icons.home),
         title: Text('Home'),
         actions: [
-          IconButton(
-            icon: Icon(Icons.notifications), 
-            onPressed: (){}
-          ),
-          IconButton(
-            icon: Icon(Icons.email), 
-            onPressed: (){}
-          ),
-          IconButton(
-            icon: Icon(Icons.camera), 
-            onPressed: (){}
-          )
+          IconButton(icon: Icon(Icons.notifications), onPressed: () {}),
+          IconButton(icon: Icon(Icons.email), onPressed: () {}),
+          IconButton(icon: Icon(Icons.camera), onPressed: () {})
         ],
       ),
       body: Center(
         child: Text('Welcome'),
       ),
+      drawer: SafeArea(
+          child: Drawer(
+        child: ListView(
+          children: [
+            UserAccountsDrawerHeader(
+              currentAccountPicture: CircleAvatar(
+                backgroundImage: AssetImage('assets/images/avatar.jpg'),
+              ),
+              accountName: Text('Samit Koyom'),
+              accountEmail: Text('samit@gmail.com'),
+              decoration: BoxDecoration(
+                  color: Colors.deepPurple,
+                  image: DecorationImage(
+                      image: AssetImage('assets/images/greenbg3.jpg'),
+                      fit: BoxFit.fill)),
+            ),
+            ListTile(
+              leading: Icon(Icons.home),
+              title: Text('หน้าหลัก'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.new_releases),
+              title: Text('ข่าวสาร'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.account_box),
+              title: Text('บัญชีผู้ใช้'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: Icon(Icons.exit_to_app),
+              title: Text('ออกจากระบบ'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      )
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        backgroundColor: Colors.teal,
+        currentIndex: 0,
+        type: BottomNavigationBarType.fixed,
+        items: <BottomNavigationBarItem> [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, color: Colors.white,),
+            title: Text('หน้าหลัก', style: TextStyle(color: Colors.white),)
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.new_releases, color: Colors.white,),
+            title: Text('ข่าวสาร',style: TextStyle(color: Colors.white))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.notifications, color: Colors.white,),
+            title: Text('แจ้งเตือน',style: TextStyle(color: Colors.white))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.pie_chart, color: Colors.white,),
+            title: Text('รายงาน',style: TextStyle(color: Colors.white))
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle, color: Colors.white,),
+            title: Text('บัญชี',style: TextStyle(color: Colors.white))
+          ),
+        ]
+      ),
+
     );
   }
 }
